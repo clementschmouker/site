6a3c859a342d58043f1ce0a5e7a99cba143481ee
clement <clement.schmouker@gmail.com>
2018-06-14 22:23:40 +0200

    started working on content

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,9 +10,10 @@
 
 
 
-    <section class="ctn">
-        <div class="ctn__inner">
-            test
+    <section class="home__section | ctn">
+        <div class="home__section__inner | ctn__inner">
+            <h2 class="home__section__title">Wanna check my work ?</h2>
+            <p class="home__section__text">If you aren't here by mistake, I bet this is why you came. If not, well, enjoy the ride.</p>
         </div>
     </section>
 
