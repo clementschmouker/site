821ca0c1cc721c3b43847e79335d3ef5b9ed5adb
clement <clement.schmouker@gmail.com>
2018-06-14 18:37:49 +0200

    grid now works partially, still forced to use a ctn__inner element for correct placement

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,10 +2,18 @@
 
 @section('content')
 
-    <header class="header | ctn">
+    <header class="header">
         <div class="header__inner">
             <h1 class="header__title">Clément Schmouker <span>Web Developer</span></h1>
         </div>
     </header>
+
+
+
+    <section class="ctn">
+        <div class="ctn__inner">
+            test
+        </div>
+    </section>
 
 @endsection
