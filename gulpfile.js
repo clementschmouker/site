d7887294f6ec80a18684d4796ad08106563cad37
clement <clement.schmouker@gmail.com>
2018-06-14 11:17:30 +0200

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,28 @@ elixir((mix) => {
         // copy files
         .copy('resources/assets/img', 'public/img')
         .copy('resources/assets/videos', 'public/videos')
+
+    	// versioning
+    	.version([
+            'public/css/style.css',
+            'public/css/admin.css',
+            'public/js/all.js',
+            'public/js/admin.js'
+        ])
+
+        // sync
+        .browserSync({
+            files: [
+                //'app/**/*',
+                'public/build/*',
+                //'public/**/*',
+                //'resources/views/**/*',
+                'resources/views/admin/*',
+                //'resources/lang/**/*'
+            ],
+            open: false,
+            logPrefix: 'yann',
+            proxy: 'yannferrandin.loc',
+            notify: false
+        });
 })
